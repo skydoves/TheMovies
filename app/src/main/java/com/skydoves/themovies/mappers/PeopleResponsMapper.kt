be6889ed5675ee74cc72b40fc8e59597aceeab4b
skydoves <skydoves@naver.com>
2018-08-15 21:47:09 +0900

    added PeopleRepository & PeopleResponseMapper

---
+++ app/src/main/java/com/skydoves/themovies/mappers/PeopleResponsMapper.kt
@@ -0,0 +1,16 @@
+package com.skydoves.themovies.mappers
+
+import com.skydoves.themovies.models.PeopleResponse
+import timber.log.Timber
+
+/**
+ * Developed by skydoves on 2018-08-15.
+ * Copyright (c) 2018 skydoves rights reserved.
+ */
+
+class PeopleResponsMapper: NetworkResponseMapper<PeopleResponse> {
+    override fun onLastPage(response: PeopleResponse): Boolean {
+        Timber.d("loadPage : ${response.page}/${response.total_pages}")
+        return response.page > response.total_pages
+    }
+}
