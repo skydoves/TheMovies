101d4a52fba187f56426259785514b336a98f18d
skydoves <skydoves@naver.com>
2018-08-17 02:16:32 +0900

    added MovieService, modify Api class

--- app/src/main/java/com/skydoves/themovies/api/Api.kt
+++ app/src/main/java/com/skydoves/themovies/api/Api.kt
@@ -6,8 +6,11 @@ package com.skydoves.themovies.api
  */
 
 object Api {
+    private val YOUTUBE = "YouTube"
     private const val BASE_POSTER_PATH = "http://image.tmdb.org/t/p/w342"
     private const val BASE_BACKDROP_PATH = "http://image.tmdb.org/t/p/w780"
+    private const val YOUTUBE_VIDEO_URL = "http://www.youtube.com/watch?v=%1"
+    private const val YOUTUBE_THUMBNAIL_URL = "http://img.youtube.com/vi/%1"
 
     fun getPosterPath(posterPath: String): String {
         return BASE_POSTER_PATH + posterPath
@@ -15,5 +18,13 @@ object Api {
 
     fun getBackdropPath(backdropPath: String): String {
         return BASE_BACKDROP_PATH + backdropPath
+    }
+
+    fun getYoutubeVideoPath(videoPath: String): String {
+        return YOUTUBE_VIDEO_URL + videoPath
+    }
+
+    fun getYoutubeThumbnailPath(thumbnailPath: String): String {
+        return YOUTUBE_THUMBNAIL_URL + thumbnailPath + "/0.jpg"
     }
 }
---
+++ app/src/main/java/com/skydoves/themovies/api/MovieService.kt
@@ -0,0 +1,25 @@
+package com.skydoves.themovies.api
+
+import android.arch.lifecycle.LiveData
+import com.skydoves.themovies.models.network.KeywordListResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * Developed by skydoves on 2018-08-17.
+ * Copyright (c) 2018 skydoves rights reserved.
+ */
+
+interface MovieService {
+    /**
+     * [Movie Keywords](https://developers.themoviedb.org/3/movies/get-movie-keywords)
+     *
+     * Get the keywords that have been added to a movie.
+     *
+     * @param [id] Specify the id of movie keywords.
+     *
+     * @return [KeywordListResponse] response
+     */
+    @GET("/3/movie/{movie_id}/keywords")
+    fun fetchKeywords(@Path("movie_id") id: Int): LiveData<ApiResponse<KeywordListResponse>>
+}
