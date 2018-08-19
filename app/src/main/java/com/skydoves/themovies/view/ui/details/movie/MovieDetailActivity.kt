0a1d87dbe22b1ae6745fdeecebd83a1874e49578
skydoves <skydoves@naver.com>
2018-08-19 23:55:34 +0900

    implements MovieDetailActivity, layout

--- app/src/main/java/com/skydoves/themovies/view/ui/details/movie/MovieDetailActivity.kt
+++ app/src/main/java/com/skydoves/themovies/view/ui/details/movie/MovieDetailActivity.kt
@@ -2,19 +2,21 @@ package com.skydoves.themovies.view.ui.details.movie
 
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.MenuItem
 import com.bumptech.glide.Glide
 import com.skydoves.themovies.R
 import com.skydoves.themovies.api.Api
-import com.skydoves.themovies.extension.applyToolbarMargin
-import com.skydoves.themovies.extension.observeLiveData
-import com.skydoves.themovies.extension.requestGlideListener
-import com.skydoves.themovies.extension.simpleToolbarWithHome
+import com.skydoves.themovies.extension.*
 import com.skydoves.themovies.models.*
 import com.skydoves.themovies.models.entity.Movie
 import com.skydoves.themovies.utils.KeywordListMapper
+import com.skydoves.themovies.view.adapter.VideoListAdapter
+import com.skydoves.themovies.view.viewholder.VideoListViewHolder
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_movie_detail.*
 import kotlinx.android.synthetic.main.layout_detail_body.*
@@ -27,10 +29,12 @@ import javax.inject.Inject
  * Copyright (c) 2018 skydoves rights reserved.
  */
 
-class MovieDetailActivity : AppCompatActivity() {
+class MovieDetailActivity : AppCompatActivity(), VideoListViewHolder.Delegate {
 
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private val viewModel by lazy { ViewModelProviders.of(this, viewModelFactory).get(MovieDetailViewModel::class.java) }
+
+    private val videoAdapter by lazy { VideoListAdapter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
@@ -56,6 +60,9 @@ class MovieDetailActivity : AppCompatActivity() {
         detail_header_title.text = getMovieFromIntent().title
         detail_header_release.text = "Release Date : ${getMovieFromIntent().release_date}"
         detail_header_star.rating = getMovieFromIntent().vote_average / 2
+        detail_body_recyclerView_trailers.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        detail_body_recyclerView_trailers.adapter = videoAdapter
+        detail_body_summary.text = getMovieFromIntent().overview
     }
 
     private fun observeViewModel() {
@@ -71,18 +78,45 @@ class MovieDetailActivity : AppCompatActivity() {
 
     private fun updateKeywordList(resource: Resource<List<Keyword>>) {
         when(resource.status) {
-            Status.SUCCESS -> detail_header_tags.tags = KeywordListMapper.mapToStringList(resource.data!!)
+            Status.SUCCESS -> {
+                detail_body_tags.tags = KeywordListMapper.mapToStringList(resource.data!!)
+
+                if(resource.data.isNotEmpty()) {
+                    detail_body_tags.visible()
+                }
+            }
             Status.ERROR -> toast(resource.errorEnvelope?.status_message.toString())
             Status.LOADING -> { }
         }
     }
 
     private fun updateVideoList(resource: Resource<List<Video>>) {
+        when(resource.status) {
+            Status.SUCCESS -> {
+                videoAdapter.addVideoList(resource)
 
+                if(resource.data?.isNotEmpty()!!) {
+                    detail_body_trailers.visible()
+                    detail_body_recyclerView_trailers.visible()
+                }
+            }
+            Status.ERROR -> toast(resource.errorEnvelope?.status_message.toString())
+            Status.LOADING -> { }
+        }
     }
 
     private fun updateReviewList(resource: Resource<List<Review>>) {
+        when(resource.status) {
+            Status.SUCCESS -> {
 
+                if(resource.data?.isNotEmpty()!!) {
+                    detail_body_reviews.visible()
+                    detail_body_recyclerView_reviews.visible()
+                }
+            }
+            Status.ERROR -> toast(resource.errorEnvelope?.status_message.toString())
+            Status.LOADING -> { }
+        }
     }
 
     private fun getMovieFromIntent(): Movie {
@@ -92,5 +126,10 @@ class MovieDetailActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if(item?.itemId == android.R.id.home) onBackPressed()
         return false
+    }
+
+    override fun onItemClicked(video: Video) {
+        val playVideoIntent = Intent(Intent.ACTION_VIEW, Uri.parse(Api.getYoutubeVideoPath(video.key)))
+        startActivity(playVideoIntent)
     }
 }
