2a3e89f8b71c02ce4074ffe2171001a2c681d840
skydoves <skydoves@naver.com>
2019-01-31 13:06:08 +0900

    UI unit test - MovieDetailActivityTest

--- app/src/androidTest/java/com/skydoves/themovies/TestTheMoviesApplication.kt
+++ app/src/androidTest/java/com/skydoves/themovies/TestTheMoviesApplication.kt
@@ -1,14 +1,26 @@
 package com.skydoves.themovies
 
-import android.app.Application
+import com.skydoves.themovies.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
 
 /**
  * Developed by skydoves on 2018-08-10.
  * Copyright (c) 2018 skydoves rights reserved.
  */
 
-class TestTheMoviesApplication : Application() {
+class TestTheMoviesApplication : DaggerApplication() {
+
+    private val appComponent = DaggerAppComponent.builder()
+            .application(this)
+            .build()
+
     override fun onCreate() {
         super.onCreate()
+        appComponent.inject(this)
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return appComponent
     }
 }
---
+++ app/src/androidTest/java/com/skydoves/themovies/view/ui/main/MovieDetailActivityTest.kt
@@ -0,0 +1,100 @@
+package com.skydoves.themovies.view.ui.main
+
+import android.os.SystemClock
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withClassName
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.skydoves.themovies.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MovieDetailActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun movieDetailActivityTest() {
+        val relativeLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.recyclerView),
+                                childAtPosition(
+                                        withClassName(`is`("android.widget.RelativeLayout")),
+                                        0)),
+                        0),
+                        isDisplayed()))
+        SystemClock.sleep(2000)
+        relativeLayout.perform(click())
+
+        val textView = onView(
+                allOf(withId(R.id.detail_body_trailers), withText("Trailers"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                                        1),
+                                0),
+                        isDisplayed()))
+        SystemClock.sleep(2000)
+        textView.check(matches(withText("Trailers")))
+
+        val textView2 = onView(
+                allOf(withText("Summary"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                                        1),
+                                2),
+                        isDisplayed()))
+        textView2.check(matches(withText("Summary")))
+
+        val appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.movie_detail_toolbar),
+                                        childAtPosition(
+                                                withContentDescription("Aquaman"),
+                                                1)),
+                                1),
+                        isDisplayed()))
+        appCompatImageButton.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>,
+        position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent) &&
+                        view == parent.getChildAt(position)
+            }
+        }
+    }
+}
