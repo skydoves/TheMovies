e59013d46474e8cd155065930271597a51476043
skydoves <skydoves@naver.com>
2019-06-07 16:17:29 +0900

    added license on the codes header

--- app/src/main/java/com/skydoves/themovies/api/RequestInterceptor.kt
+++ app/src/main/java/com/skydoves/themovies/api/RequestInterceptor.kt
@@ -1,13 +1,31 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Designed and developed by 2018 skydoves (Jaewoong Eum)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.skydoves.themovies.api
 
 import com.skydoves.themovies.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
-
-/**
- * Developed by skydoves on 2018-08-11.
- * Copyright (c) 2018 skydoves rights reserved.
- */
 
 internal class RequestInterceptor : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
