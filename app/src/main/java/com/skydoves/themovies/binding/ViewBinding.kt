a6148cb48d555ac45f1f62dd86c094a2b5aa46a9
skydoves <skydoves@naver.com>
2019-06-08 22:32:08 +0900

    implements ViewBinding for binding data to views

---
+++ app/src/main/java/com/skydoves/themovies/binding/ViewBinding.kt
@@ -0,0 +1,78 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Designed and developed by 2018 skydoves (Jaewoong Eum)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.skydoves.themovies.binding
+
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import co.lujun.androidtagview.TagContainerLayout
+import com.bumptech.glide.Glide
+import com.skydoves.themovies.api.Api
+import com.skydoves.themovies.extension.bindResource
+import com.skydoves.themovies.extension.requestGlideListener
+import com.skydoves.themovies.extension.visible
+import com.skydoves.themovies.models.Keyword
+import com.skydoves.themovies.models.Resource
+import com.skydoves.themovies.models.entity.Movie
+import com.skydoves.themovies.utils.KeywordListMapper
+
+@BindingAdapter("visibilityByResource")
+fun bindVisibilityByResource(view: View, resource: Resource<List<Any>>?) {
+  view.bindResource(resource) {
+    if (resource?.data?.isNotEmpty()!!) {
+      view.visible()
+    }
+  }
+}
+
+@BindingAdapter("mapKeywordList")
+fun bindMapKeywordList(view: TagContainerLayout, resource: Resource<List<Keyword>>?) {
+  view.bindResource(resource) {
+    view.tags = KeywordListMapper.mapToStringList(resource?.data!!)
+    if (resource.data.isNotEmpty()) {
+      view.visible()
+    }
+  }
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("bindReleaseDate")
+fun bindReleaseDate(view: TextView, movie: Movie) {
+  view.text = "Release Date : ${movie.release_date}"
+}
+
+@BindingAdapter("bindBackDrop")
+fun bindBackDrop(view: ImageView, movie: Movie) {
+  if (movie.backdrop_path != null) {
+    Glide.with(view.context).load(Api.getBackdropPath(movie.backdrop_path))
+      .listener(view.requestGlideListener())
+      .into(view)
+  } else {
+    Glide.with(view.context).load(Api.getBackdropPath(movie.poster_path!!))
+      .listener(view.requestGlideListener())
+      .into(view)
+  }
+}
