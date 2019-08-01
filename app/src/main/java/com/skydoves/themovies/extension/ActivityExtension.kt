924770064d0280014ae58a50bad5823b7bbb5dfd
skydoves <skydoves@naver.com>
2019-08-01 20:08:08 +0900

    rearrange & cleanup

--- app/src/main/java/com/skydoves/themovies/extension/ActivityExtension.kt
+++ app/src/main/java/com/skydoves/themovies/extension/ActivityExtension.kt
@@ -30,23 +30,15 @@ import android.graphics.drawable.Drawable
 import android.os.Build
 import android.view.View
 import android.view.ViewAnimationUtils
-import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
-import androidx.databinding.ViewDataBinding
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.skydoves.themovies.R
-import com.skydoves.themovies.delegate.ActivityBindingProperty
-import com.skydoves.themovies.delegate.ViewModelDelegate
-import kotlin.reflect.KClass
 
 fun checkIsMaterialVersion() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
 
