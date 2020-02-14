dfd21cd038e213418ab5030c57e8c7deb8185a0e
Pravin-Divraniya <pgdivraniya@gmail.com>
2020-02-14 17:56:25 +0530

    UI enhancemnt in Movie and TV list screen. Also resolve one crash.

--- app/src/main/java/com/skydoves/themovies/models/network/PersonDetail.kt
+++ app/src/main/java/com/skydoves/themovies/models/network/PersonDetail.kt
@@ -32,7 +32,7 @@ import kotlinx.android.parcel.Parcelize
 data class PersonDetail(
   val birthday: String,
   val known_for_department: String,
-  val place_of_birth: String,
+  val place_of_birth: String?,
   val also_known_as: List<String>,
   val biography: String
 ) : Parcelable, NetworkResponseModel
