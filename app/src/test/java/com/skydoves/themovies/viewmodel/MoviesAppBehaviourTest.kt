5825cf553a4e87608e9242fa8f2535997752f00a
Maurice HUNLEDE <hunledemaurice@gmail.com>
2023-06-27 22:19:50 +0000

    added : MoviesAppBehaviourTest class to perform the test cases

---
+++ app/src/test/java/com/skydoves/themovies/viewmodel/MoviesAppBehaviourTest.kt
@@ -0,0 +1,20 @@
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import com.example.movies.MainActivity
+import com.example.movies.R
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MoviesAppBehaviourTest {
+    
+}
